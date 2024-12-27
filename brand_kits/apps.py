80a2391864d3ade00db6f3ecc703ff5d1ebda09d
elleon003 <noelle@ygitc.com>
2024-12-26 23:03:37 -0500

    Updated client model, added assets, brand kits, core apps

---
+++ brand_kits/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BrandKitsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'brand_kits'
--- client/models.py
+++ client/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ClientContact(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
